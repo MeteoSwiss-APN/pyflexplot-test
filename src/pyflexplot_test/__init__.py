59c253d7da756da2483e2c5a3b1f29209cb758d5
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-04-21 21:29:26 +0200

    new version v0.3.1 (patch bump)
    create diff animation

--- src/pyflexplot_test/__init__.py
+++ src/pyflexplot_test/__init__.py
@@ -8,6 +8,6 @@ from typing import List
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__: List[str] = []
