aab045db4dd580e624b6e94da9faf2fe26a175e6
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-05-05 14:05:31 +0200

    new version v0.4.10 (patch bump)
    fix various path-related issues; disable progress counter in case of output redirection

--- src/pyflexplot_test/__init__.py
+++ src/pyflexplot_test/__init__.py
@@ -8,6 +8,6 @@ from typing import List
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.4.9"
+__version__ = "0.4.10"
 
 __all__: List[str] = []
